4bd527d9613c9902e6863ad2a0d86c0e5d76f48e
rlegan <legan.renaud@gmail.com>
2022-06-08 23:48:37 +0200

    first commit

---
+++ src/bot.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+from constants import TOKEN
+
+bot = commands.Bot(command_prefix="z!")
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "bruh moment" in message.content:
+        await message.channel.send("bruh moment indeed")
+    elif "bruh" in message.content:
+        await message.channel.send("bruh")
+    elif "hurb" in message.content:
+        await message.channel.send("hurb")
+
+
+bot.run(TOKEN)
